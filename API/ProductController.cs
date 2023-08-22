634fa69add0f7c6a99bf482f0c406a4432245f82
nabruna <brunanegrellideandrade@gmail.com>
2023-08-21 21:31:14 -0300

    whoop

---
+++ API/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+[ApiController]
+//define controller path
+[Route("/product")]
+public class ProductController : ControllerBase
+{
+    [Route("list")]
+    [HttpGet]
+    public string List(){
+        return "Hello world";
+    }
+}
